36ff73c77d554b13f0739f8a274d2fcae5336ecb
spatrickdoyle <sean@spatrickdoyle.com>
2017-03-30 22:32:45 -0500

    Now using a complex (correlative) normal distribution. It is catagorizing some of the data 100% effectively, but some of it is too similar, probably due to measurement error

--- models.py
+++ models.py
@@ -1,137 +1,159 @@
-import time,math,glob
+import math
 import numpy as np
-#import matplotlib.patches as mpatches
-import matplotlib.pyplot as plt
 import scipy.special as sp
-#from sklearn import svm
-#from sklearn.ensemble import RandomForestClassifier
-#import regression as r
-#import analysis
 
+#Create plots from 'average' values
+#Measure the percent change in error for each added coefficient and weight the sum
+#Create an accurate distribution of 0th order coefficients
 
 PI = 3.1415926535
 E = 2.71828
+
+#Heaviside step function, half-maximum convention
 step = lambda x: 0.0 if x < 0 else 1.0 if x > 0 else 0.5
 
+#General formula for calculating the nth order Fourier coefficient
 def c_(n,m,T,w,X,Y):
     return -(1/(1j*n*w*T))*sum([((Y[j]-Y[j-1])/(X[j]-X[j-1]))*(X[j]*(E**(-1j*n*w*X[j])) - X[j-1]*(E**(-1j*n*w*X[j-1]))) + (E**(-1j*n*w*X[j]) - E**(-1j*n*w*X[j-1]))*(Y[j]-((Y[j]-Y[j-1])/(X[j]-X[j-1]))*X[j]+(1/(1j*n*w))*((Y[j]-Y[j-1])/(X[j]-X[j-1]))) for j in range(1,m)])
 
+def c0_(m,T,X,Y):
+    return (1/(2*T))*sum([(Y[j]-Y[j-1])*(X[j-1]-X[j])+Y[j] for j in range(1,m)])
+
+#General formula for finding the probability a value falls within a normal distribution
 def f_(x,mu,sig):
-    return 1 - 0.5*( sp.erf(np.abs(x-mu)/(sig*math.sqrt(2))) - sp.erf(-np.abs(x-mu)/(sig*math.sqrt(2))) )
-
-class Classification:
-    def __init__(self,classification):
-        if not isinstance(classification,list):
-            self.classification = [[classification for j in range(8)] for i in range(8)]
-        else:
-            self.classification = classification
-
-    def set(self,i,j,clas):
-        self.classification[i][j] = clas
-
-    def get(self,i,j):
-        return self.classification[i][j]
+    #return 1 - 0.5*( sp.erf(np.abs(x-mu)/(sig*math.sqrt(2))) - sp.erf(-np.abs(x-mu)/(sig*math.sqrt(2))) )
+    #return (1/math.sqrt(2*PI*(sig**2)))*E**(-((x-mu)**2)/(2*(sig**2)))
+    return (E**(-(np.absolute(x)**2)/(2*(sig**2))))/(2*PI*(sig**2))
+        
 
 class Sweep:
-    def __init__(self,capfile,cndfile,classification=None):
-        capFile = file(capfile)
-        cndFile = file(cndfile)
+    def __init__(self,fileName,size):
+        self.size = size #The size of the matrix to load and store
 
-        #Construct the sweep matrices - an 8x8 matrix, where each element is a 401 length array (a sweep)
+        #Open the two files, which should have the exact same path except with 'cap' or 'con' in the placeholder
+        capFile = file(fileName%'cap') #Capacitance data
+        conFile = file(fileName%'con') #Conductivity data
+
+        #Construct the sweep matrices - 8x8 matrix, where each element is a 401 length array (a sweep)
         data = []
         for line in capFile.readlines():
             data.append([float(i) for i in line.split(',')])
-        self.capacitance = [[[] for j in range(8)] for i in range(8)]
+        size = int(math.sqrt(len(data)))
+        capData = [[[] for j in range(size)] for i in range(size)]
         for sweep in range(len(data)):
-            self.capacitance[sweep%8][sweep/8] = data[sweep]
+            capData[sweep%size][sweep/size] = data[sweep]
 
         data = []
-        for line in cndFile.readlines():
+        for line in conFile.readlines():
             data.append([float(i) for i in line.split(',')])
-        self.conductivity = [[[] for j in range(8)] for i in range(8)]
+        size = int(math.sqrt(len(data)))
+        conData = [[[] for j in range(size)] for i in range(size)]
         for sweep in range(len(data)):
-            self.conductivity[sweep%8][sweep/8] = data[sweep]
-
-        #For now just use the first intersection
-        #self.capacitance = [[[float(i) for i in capFile.readline().split(',')]]]
-        #self.conductivity = [[[float(i) for i in cndFile.readline().split(',')]]]
+            conData[sweep%size][sweep/size] = data[sweep]
 
         capFile.close()
-        cndFile.close()
+        conFile.close()
 
-        #Generate complex data set
-        self.data = [[[self.capacitance[i][j][k]+(self.conductivity[i][j][k]*1j) for k in range(len(self.capacitance[i][j]))] for j in range(len(self.capacitance[i]))] for i in range(len(self.capacitance))]
+        #Generate complex data subset using all the data just loaded
+        self.data = [[[capData[i][j][k]+(conData[i][j][k]*1j) for k in range(len(capData[i][j]))] for j in range(self.size)] for i in range(self.size)]
 
-        #The classification, if there is one
-        self.classification = classification
 
-    def setClass(self,c):
-        #set the classification for this sweep
-        self.classification = c
+        #Generate a matrix of classifications (a separate list for each intersection) as well as the likelihoods of each classification
+
+        self.classifications = [[[] for i in range(size)] for j in range(size)] #Matrix of possible classifications (lists of strings)
+        self.likelihoods = [[[] for i in range(size)] for j in range(size)] #Matrix of the likelihoods for each classification in the classifications matrix (list of floats 0.0-1.0)
+
+    def addClass(self,i,j,clas,likelihood):
+        #Add a possible classification and its likelihood to a single index of the matrix
+        self.classifications[i][j].append(clas)
+        self.likelihoods[i][j].append(likelihood)
+
+    def getClass(self,i,j):
+        return self.classifications[i][j][self.likelihoods[i][j].index(max(self.likelihoods[i][j]))]
+
+    def genClass(self):
+        return [[self.getClass(i,j) for j in range(self.size)] for i in range(self.size)]
 
     def c(self,n):
         #return order n coefficients for this sweep as a matrix of complex numbers
         m = 401
         X = range(401)
         C = []
-        for i in self.data:
-            tmp = []
-            for Y in i:
-                T = X[m-1]-X[0]
-                w = (2*PI)/T
-                tmp.append(c_(n,m,T,w,X,Y))
-            C.append(tmp)
-        return C
-
-    def g(self,x,order):
-        m = 401
-        X = range(401)
         T = X[m-1]-X[0]
         w = (2*PI)/T
-        return sum([ (self.c(n)[0][0])*(E**(1j*n*w*x)) for n in range(-order,0)]) + sum([ (self.c(n)[0][0])*(E**(1j*n*w*x)) for n in range(1,order+1)])
+        if n != 0:
+            for i in self.data:
+                tmp = []
+                for Y in i:
+                    tmp.append(c_(n,m,T,w,X,Y))
+                C.append(tmp)
+        else:
+            for i in self.data:
+                tmp = []
+                for Y in i:
+                    #tmp.append(Y[0])
+                    #print c0_(m,T,X,Y)
+                    tmp.append(c0_(m,T,X,Y))
+                C.append(tmp)
+        return C
+
 
 class Model:
     #Each model is loaded with sweeps from the same classification and generates probabilities that sweeps belong to that given classification
-    def __init__(self,order,classification):
-        #The model itself is an 8x8 matrix, and each index is a list of complex mu-sigma tuples
-        self.order = order
+    def __init__(self,order,clas):
+        #The model itself is a max 8x8 matrix, and each index is a list of complex mu-sigma tuples
         self.model = [[[(0,0) for i in range(order)] for j in range(8)] for k in range(8)]
-        self.classification = classification
+
+        self.order = order #The order of the model (the maximum n value for generated coefficients)
+        self.classification = clas #String representation of the classification of this model
+
+        self.rang = (1,self.order+1) #Exclusive range for orders of coefficients to use (so that we can easily include or exclude 0)
 
     def train(self,sweeps):
         #Train each point in the models using an array of sweep objects
 
+        print "Training %s model..."%self.classification
+
         #generate the model for each sweep
-        for k in range(8):
-            for l in range(8):
-                print k,l
-                for j in range(self.order):
-                    coefs = [s.c(j+1)[k][l] for s in sweeps]
+        for row in range(sweeps[0].size):
+            for col in range(sweeps[0].size):
+                for order in range(self.rang[0],self.rang[1]):
+                    #Generate a list of coefficients of a specific order for each sweep
+                    coefs = [s.c(order)[row][col] for s in sweeps]
+                    #Find the mean of this nth order coefficient
                     mean = sum(coefs)/len(coefs)
-                    sigma = np.sqrt(sum([np.real(i)**2 for i in coefs])/len(coefs) - mean**2)
-                    self.model[k][l][j] = (mean,sigma)
-        #print self.model
+                    #And the standard deviation
+                    #sigma = np.sqrt(sum([i**2 for i in coefs])/len(coefs) - mean**2)
+                    #sigma = np.sqrt(sum([np.real(i)**2 for i in coefs])/len(coefs) - mean**2)
+                    sigma = math.sqrt(0.5*(sum([np.real(i)**2 for i in coefs])/len(coefs) + sum([np.real(i)**2 for i in coefs])/len(coefs)))
+
+                    #Save it into a big matrix
+                    self.model[row][col][order-1] = (mean,sigma)
 
     def test(self,sweeps):
         #Return a list of probabilities, one for each sweep
-        for k in range(8):
-            for l in range(8):
-                for s in sweeps:
-                    print "Sweep:"
-                    coefs = [s.c(i+1)[k][l] for i in range(self.order)] #List of coefficients for the test sweep, where the order is the index+1
-                    means = [i[0] for i in self.model[k][l]]
-                    sigs = [i[1] for i in self.model[k][l]]
+        for s in sweeps:
+            for row in range(sweeps[0].size):
+                for col in range(sweeps[0].size):
+                    coefs = [s.c(i)[row][col] for i in range(self.rang[0],self.rang[1])] #List of coefficients for the test sweep, where the order is the index+1
+                    means = [i[0] for i in self.model[row][col]]
+                    sigs = [i[1] for i in self.model[row][col]]
 
-                    #for order in range(self.order):
-                    #print f_(-1.06322e-3,-1.03934e-3,2.647e-5)
-                    #print coefs[order],means[order],sigs[order]
-                    #print f_(np.real(coefs[order]),np.real(means[order]),sigs[order])
-                    print k,l,sum([np.real(f_(np.real(coefs[order]),np.real(means[order]),sigs[order])) for order in range(self.order)])/self.order
-            #print ''
+                    likelihood = sum([f_(coefs[order],means[order],sigs[order]) for order in range(self.rang[0]-1,self.rang[1]-1)])/self.order
+                    #likelihood = sum([np.real(f_(np.real(coefs[order]),np.real(means[order]),sigs[order])) for order in range(self.rang[0]-1,self.rang[1]-1)])/self.order
 
-    def g(self,x,order):
-        m = 401
-        X = range(401)
+                    s.addClass(row,col,self.classification,likelihood)
+                    #print row,col,s.likelihoods[row][col][-1]
+
+    def avgPlot(self,x):
+        #Function using the mean coefficients of the first index of the model
+        m = 4
+        X = range(m)
         T = X[m-1]-X[0]
         w = (2*PI)/T
-        return sum([ (-self.model[0][0][-(n+1)][0])*(E**(1j*n*w*x)) for n in range(-order,0)]) + sum([ (self.model[0][0][n-1][0])*(E**(1j*n*w*x)) for n in range(1,order+1)])
+
+        print "Mean %d"%x
+        print sum([ (-np.real(self.model[0][0][-(n+1)][0]) + np.imag(self.model[0][0][-(n+1)][0])*1j)*(E**(1j*n*w*x)) for n in range(-self.order,0)]),sum([ (self.model[0][0][n-1][0])*(E**(1j*n*w*x)) for n in range(1,self.order+1)])
+        print sum([ (-np.real(self.model[0][0][-(n+1)][0]) + np.imag(self.model[0][0][-(n+1)][0])*1j)*(E**(1j*n*w*x)) for n in range(-self.order,0)])+sum([ (self.model[0][0][n-1][0])*(E**(1j*n*w*x)) for n in range(1,self.order+1)])
+
+        return sum([ (-np.real(self.model[0][0][-(n+1)][0]) + np.imag(self.model[0][0][-(n+1)][0])*1j)*(E**(1j*n*w*x)) for n in range(-self.order,0)]) + sum([ (self.model[0][0][n-1][0])*(E**(1j*n*w*x)) for n in range(1,self.order+1)])
